0dbd6bfe4592de219dfb8ae3ce45e49798ca9c4b
Paul Kosemjako <kosemjako.paul@gmail.com>
2025-05-28 21:55:45 +0200

    Routes GitHub

--- src/pages/generateGalleryData.cjs
+++ src/pages/generateGalleryData.cjs
@@ -16,7 +16,7 @@ fs.readdirSync(galleryDir).forEach((folderName) => {
 
     images.forEach(image => {
       data.push({
-        src: `/images/gallery/${folderName}/${image}`,
+        src: `/luises-seeblick/images/gallery/${folderName}/${image}`,
         alt: `${folderName} - ${image}`,
         category: folderName
       });
